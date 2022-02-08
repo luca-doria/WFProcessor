303027ac964cfc977202de575a73a4a75c8b689f
Luca Doria <92083146+luca-doria@users.noreply.github.com>
2022-02-08 11:17:19 +0100

    Add files via upload

---
+++ Linkdef.h
@@ -0,0 +1,12 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+#pragma link C++ class Waveform;
+#pragma link C++ class WaveformProcessor;
+#pragma link C++ class LecroyFile;
+//#pragma link C++ class LecroyHdfFile;
+#pragma link C++ class V1730File;
+
+#endif
